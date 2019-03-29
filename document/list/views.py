131e6a3c4efdfb7ac4766b8569c45cb9676791b5
ThanhHung69 <thanhhungsky98@gmail.com>
2019-03-30 00:22:49 +0700

    Hung
    Update Pagination

--- document/list/views.py
+++ document/list/views.py
@@ -3,6 +3,7 @@ from django.shortcuts import render, redirect, get_object_or_404
 from .models import *
 from django.db.models import Q
 from .forms import *
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.core.files.storage import FileSystemStorage
 # Create your views here.
 
@@ -11,12 +12,21 @@ def display_student(request):
     students = Student.objects.all()
     if query  :
         students = Student.objects.filter(Q(id_student__icontains=query)|Q(topic__icontains=query)|Q(name__icontains=query))
+        paginator = Paginator(students, 10)
+
+        page = request.GET.get('page')
+        students = paginator.get_page(page)
         return render(request, 'list/list.html', {'students':students})
     else:
         return render(request, 'list/searchpage.html', {'students':students})
 
 def view(request):
     students = Student.objects.all()
+    paginator = Paginator(students, 20)  # Show 20 students per page
+
+    page = request.GET.get('page')
+    students= paginator.get_page(page)
+
     return render(request, 'list/list.html', {'students':students})
 
 def add_student(request):
